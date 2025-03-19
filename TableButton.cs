5f4fb8f5b5daf7565530ae03bbd8a161a85dae03
bscott519 <113139519+bscott519@users.noreply.github.com>
2025-03-19 13:46:57 -0400

    updated repo

--- TableButton.cs
+++ TableButton.cs
@@ -1,47 +1,58 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 public class TableButton : MonoBehaviour
 {
-    private Animator animator;
+    public Animator animator;
     private enum AnimationState { Idle, Drop_Flap, Reversed_Flap }
     private AnimationState curState;
 
     void Start()
-    {
-        GetComponent<XRSimpleInteractable>().selectEntered.AddListener(x => ToggleAnimation());
-        animator = GetComponent<Animator>();
+    { 
+        XRSimpleInteractable interactable = GetComponent<XRSimpleInteractable>();
+        if (interactable != null )
+        {
+            interactable.selectEntered.AddListener(ToggleAnimation);
+        }
+
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
         curState = AnimationState.Idle;
         animator.Play("idle");
     }
 
-    public void ToggleAnimation()
+    public void ToggleAnimation(SelectEnterEventArgs args)
     {
-        if (animator != null)
+        if (animator == null) 
         {
-            switch (curState)
-            {
-                case AnimationState.Idle:
-                    animator.SetBool("PlayAnimation", true);
-                    animator.Play("flap");
-                    curState = AnimationState.Drop_Flap;
-                    break;
-
-                case AnimationState.Drop_Flap:
-                    animator.SetBool("PlayAnimation", true);
-                    animator.Play("reverse_flap");
-                    curState = AnimationState.Reversed_Flap;
-                    break;
-
-                case AnimationState.Reversed_Flap:
-                    animator.SetBool("PlayAnimation", true);
-                    animator.Play("flap");
-                    curState = AnimationState.Drop_Flap;
-                    break;
-            }
-
+            Debug.Log("animator is null");
+            return;
         }
+
+        switch (curState)
+                    {
+                        case AnimationState.Idle:
+                            //animator.SetBool("PlayAnimation", true);
+                            animator.Play("flap", 0, 0);
+                            curState = AnimationState.Drop_Flap;
+                            break;
+
+                        case AnimationState.Drop_Flap:
+                            //animator.SetBool("PlayAnimation", true);
+                            animator.Play("reverse_flap", 0, 0);
+                            curState = AnimationState.Reversed_Flap;
+                            break;
+
+                        case AnimationState.Reversed_Flap:
+                            //animator.SetBool("PlayAnimation", true);
+                            animator.Play("flap", 0, 0);
+                            curState = AnimationState.Drop_Flap;
+                            break;
+                    }
 
     }
 }
